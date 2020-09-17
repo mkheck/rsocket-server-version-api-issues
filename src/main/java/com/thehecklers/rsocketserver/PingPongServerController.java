40d205d5815e687a9ab51fa472b37ba8febf4f5f
Mark Heckler <mark.heckler@gmail.com>
2020-09-17 10:15:26 -0500

    Updates

--- src/main/java/com/thehecklers/rsocketserver/PingPongServerController.java
+++ src/main/java/com/thehecklers/rsocketserver/PingPongServerController.java
@@ -2,7 +2,10 @@ package com.thehecklers.rsocketserver;
 
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.time.Duration;
 
 @Controller
 public class PingPongServerController {
@@ -10,5 +13,16 @@ public class PingPongServerController {
     Mono<Pong> pingpong(Ping ping) {
         System.out.println("   >>> Incoming ping: " + ping.getMessage());
         return Mono.just(new Pong("Returning ->" + ping.getMessage() + "<-"));
+    }
+
+    @MessageMapping("pingpongflux")
+    Flux<Pong> pingpongflux(Flux<Ping> pingFlux) {
+        return pingFlux.map(ping -> new Pong("Returning ->" + ping.getMessage() + "<-"));
+    }
+
+    @MessageMapping("onetomany")
+    Flux<Pong> pingpongflux(Ping ping) {
+        return Flux.interval(Duration.ofSeconds(1))
+                .map(l -> new Pong("Returning ->" + ping.getMessage() + "<-"));
     }
 }
--- src/main/java/com/thehecklers/rsocketserver/RsocketServerApplication.java
+++ src/main/java/com/thehecklers/rsocketserver/RsocketServerApplication.java
@@ -2,12 +2,22 @@ package com.thehecklers.rsocketserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.netty.DisposableServer;
+import reactor.netty.tcp.TcpServer;
 
 @SpringBootApplication
 public class RsocketServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RsocketServerApplication.class, args);
+//
+//		DisposableServer server =
+//				TcpServer.create()
+//						.wiretap(true)
+//						.bindNow();
+//
+//		server.onDispose()
+//				.block();
 	}
 
 }
